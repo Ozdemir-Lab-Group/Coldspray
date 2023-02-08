e906a40a511fb871c260d69c7216b795e91bf0fb
John Curanaj <Jcuranaj24@gmail.com>
2023-02-08 18:20:39 -0500

    Added ecosystem.config file for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: "ubuntu",
+      host: "34.204.95.107",
+      ref: "origin/master",
+      repo: "https://github.com/JCur24/Coldspray.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
+};
